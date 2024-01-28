41779a3aa822e4d3aeb5ce6e39b8c86b2e5768b6
Tee6 <niko.leimegger@wolfvision.net>
2024-01-28 14:48:11 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import PersonaCard from './components/PersonaCard';
+
+function App() {
+  return (
+    <>
+    <body>
+    <div>
+        <PersonaCard/>
+      </div>
+    </body>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PersonaCard.tsx
@@ -0,0 +1,18 @@
+function GlassCard(){
+    return (
+    <div className="glassCard">
+        <div className="card">
+            <div className="card-image">
+                <img className="portrait-img" src="src\assets\Portrait.png" alt="Portrait" />
+            </div>
+            <div className="card-content">
+                <h3>Niko Leimegger</h3>
+                <p>Age: 19</p>
+                <h4>Currently working at McDonalds</h4>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default GlassCard;
