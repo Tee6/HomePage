90401998a64b040a7696c0633c51495b34ef0891
Tee6 <niko.leimegger@wolfvision.net>
2024-06-10 12:07:17 +0200

    contact me btn

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import '../styles/contactbtn.css';
 
 const ContactMe: React.FC = () => {
+    const email = 'n.leimegger@gmail.com';
+    const subject = 'Hallo Niko!';
+    const body = 'Hallo Niko! Ich habe deine Webseite gesehen und möchte gerne mit dir in Kontakt treten.';
+
+    const handleClick = () => {
+        window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    };
+
     return (
         <div className='cnt-div'>
-            <button className='contact-me-btn'>
+            <button className='contact-me-btn' onClick={handleClick}>
                 Contact Me
             </button>
         </div>
